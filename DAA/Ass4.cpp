2af3f365823c1fee74a55b12b6edc6ba0c235457
Onkar Muthe <onkarmuthe.2016@gmail.com>
2025-11-09 11:49:14 +0530

    updated n-queen

--- DAA/Ass4.cpp
+++ DAA/Ass4.cpp
@@ -3,38 +3,38 @@ using namespace std;
 
 int n;
 int start_row, start_col;
-vector<vector<string>> res;
+vector<vector<string>> solutions;
 
-void backtrack(int r, vector<string>& board,
-               unordered_set<int>& col,
-               unordered_set<int>& posDiag,
-               unordered_set<int>& negDiag) {
+void solveNQueens(int r, vector<string>& board,
+                  unordered_set<int>& cols,
+                  unordered_set<int>& posDiag,
+                  unordered_set<int>& negDiag) {
     if (r == n) {
-        res.push_back(board);
+        solutions.push_back(board);
         return;
     }
 
     // Skip the fixed starting queen row
     if (r == start_row) {
-        backtrack(r + 1, board, col, posDiag, negDiag);
+        solveNQueens(r + 1, board, cols, posDiag, negDiag);
         return;
     }
 
     for (int c = 0; c < n; c++) {
-        if (col.count(c) || posDiag.count(r + c) || negDiag.count(r - c))
+        if (cols.count(c) || posDiag.count(r + c) || negDiag.count(r - c))
             continue;
 
         // Choose
-        col.insert(c);
+        cols.insert(c);
         posDiag.insert(r + c);
         negDiag.insert(r - c);
         board[r][c] = '1';
 
-        // Explore
-        backtrack(r + 1, board, col, posDiag, negDiag);
+        // Explore next row
+        solveNQueens(r + 1, board, cols, posDiag, negDiag);
 
         // Undo choice
-        col.erase(c);
+        cols.erase(c);
         posDiag.erase(r + c);
         negDiag.erase(r - c);
         board[r][c] = '0';
@@ -46,19 +46,19 @@ void n_queens(int n_input, int s_row, int s_col) {
     start_row = s_row;
     start_col = s_col;
 
-    unordered_set<int> col, posDiag, negDiag;
+    unordered_set<int> cols, posDiag, negDiag;
     vector<string> board(n, string(n, '0'));
 
     // Place the fixed queen
-    col.insert(start_col);
+    cols.insert(start_col);
     posDiag.insert(start_row + start_col);
     negDiag.insert(start_row - start_col);
     board[start_row][start_col] = '1';
 
-    backtrack(0, board, col, posDiag, negDiag);
+    solveNQueens(0, board, cols, posDiag, negDiag);
 
-    // Print all valid solutions
-    for (auto& sol : res) {
+    cout << "\nTotal Solutions Found: " << solutions.size() << "\n\n";
+    for (auto& sol : solutions) {
         for (auto& row : sol)
             cout << row << "\n";
         cout << "\n";
